7a3913da58edc9acdf0968cf43059738de2f0899
Popcorn Dreamer <bloom.cyberix@gmail.com>
2023-03-19 23:18:10 +0400

    bruh

--- 2sum.py
+++ 2sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         length = len(nums)
-        iterations=0
-        for i in range(length):
-            iterations+=i
 
-        for i in range(iterations):
-            if ((i == iterations-1) and (nums[i-1] + nums[i] == target) and length > 2):
-                return [i-1, i]
-            if nums[i] + nums[i+1] == target:
-                return [i, i+1]
-            
+        for i in range(length - 1 ):
+            for j in range(i+1, length):
+                if(nums[i] + nums[i+1] == target):
+                    return [i, i+1]
+
+#attempt 2, not inventing smtg new this time            
