62d51d4b4e6bc382750841e09b13f08988bf8c8b
Popcorn Dreamer <bloom.cyberix@gmail.com>
2023-03-20 22:18:39 +0400

    oui oui getting there

--- longestSubstr.py
+++ longestSubstr.py
@@ -1,24 +1,29 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        sub = ""
-        sub2 = ""
-        a = list(strs[0])
-        b = list(strs[1])
-        c = list(strs[2])
-        k = 0
-        for i in range(len(a)):
-            for j in range(len(b)):
-                if(i+k) < len(a) and (j+k) < len(b) and a[i + k] == b[j + k]:
-                    sub = sub + a[i+k]
-                    k = k + 1
-        k = 0
-        for i in range(len(a)):
-            for j in range(len(c)):
-                if(i+k) < len(a) and (j+k) < len(c) and a[i + k] == c[j + k]:
-                    sub2 = sub2 + a[i+k]
-                    k = k + 1
-
-        if sub == sub2:
-            return sub
-        else:
-            return ""
+def longestCommonPrefix(strs):
+    sub = ""
+    sub2 = ""
+    a = strs[0]
+    b = strs[1]
+    c = strs[2]
+    k = 0
+    for i in range(len(a)):
+        for j in range(len(b)):
+            while((i+k) < len(a) and (j+k) < len(b) and a[i + k] == b[j + k]):
+                sub = sub + a[i+k]
+                k = k + 1
+    u = 0
+    for i in range(len(a)):
+        for j in range(len(c)):
+            while((i+u) < len(a) and (j+u) < len(c) and a[i + u] == c[j + u]):
+                sub2 = sub2 + a[i+u]
+                u = u + 1
+    
+    if sub == sub2:
+        print(sub2)
+        return sub
+    if sub in sub2:
+        return sub
+    elif sub2 in sub:
+        return sub2
+    return ""
+    
+print(longestCommonPrefix(['flower', 'flight', 'flow']))
