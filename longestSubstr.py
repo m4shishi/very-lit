c47abf8f78a3cde0fea5bc7e276d6574240b21dd
Popcorn Dreamer <bloom.cyberix@gmail.com>
2023-03-20 22:04:51 +0400

    y am i so close yet so far smtms

--- longestSubstr.py
+++ longestSubstr.py
@@ -1,34 +1,24 @@
-def longestCommonPrefix(strs):
-    sub = ""
-    sub2 = ""
-    a = list(strs[0])
-    b = list(strs[1])
-    c = list(strs[2])
-    for i in range(len(a)):
-        for j in range(len(b)):
-            if a[i] == b[j]:
-                sub = sub + a[i]
-                print(sub)
-            # else:
-            #     break
-    
-    for i in range(len(a)):
-        for j in range(len(c)):
-            if a[i] == c[j]:
-                #print("a[i]=",a[i])
-                sub2 = sub2 + a[i]
-                #print(sub2)
-            else:
-                sub2 = ""
-    if sub in a and sub in b:
-        print("we got ",sub)
-    else:
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         sub = ""
-        print(sub)
-        
-    # if sub == sub2:
-    #     return sub
-    # else:
-    #     return ""
-  
-longestCommonPrefix(['flowers','flows','fleas'])
+        sub2 = ""
+        a = list(strs[0])
+        b = list(strs[1])
+        c = list(strs[2])
+        k = 0
+        for i in range(len(a)):
+            for j in range(len(b)):
+                if(i+k) < len(a) and (j+k) < len(b) and a[i + k] == b[j + k]:
+                    sub = sub + a[i+k]
+                    k = k + 1
+        k = 0
+        for i in range(len(a)):
+            for j in range(len(c)):
+                if(i+k) < len(a) and (j+k) < len(c) and a[i + k] == c[j + k]:
+                    sub2 = sub2 + a[i+k]
+                    k = k + 1
+
+        if sub == sub2:
+            return sub
+        else:
+            return ""
